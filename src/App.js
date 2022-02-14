d9619769c02bfd243ad1924e3c584d6cd9ff21bd
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-14 21:26:27 +0900

    class 라이프 사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { Component } from 'react/cjs/react.production.min';
 import './App.css';
-import Say from './Say';
+import LifeCycleSample from './lifeCycleSample/LifeCycleSample';
 
-const App = () => {
-  
-  return (
-    <div className='react'>
-      <Say/>
-    </div>
-    );
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/EventPractice.js
@@ -0,0 +1,56 @@
+import {useState} from 'react';
+
+const EventPractice = () => {
+  const [form, setForm] = useState(
+    {
+      username: '',
+      message: ''
+    }
+  );
+  const { username, message } = form;
+  const onChange = e => {
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value
+    };
+    setForm(nextForm);
+  }
+
+  const onClick = () => {
+    alert(`${username} : ${message}`);
+    setForm({
+      username: '',
+      message: ''
+    });
+  };
+
+  const onkeyPress = e => {
+    if (e.key === "Enter") {
+      onClick();
+    }
+  }
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="입력하세요"
+        value={message}
+        onChange={onChange}
+        onkeyPress={onkeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
+
+export default EventPractice;
