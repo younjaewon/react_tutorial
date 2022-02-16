45000b3760cb4cc9d1938654ebde61151929efa3
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-16 21:18:55 +0900

    hook state effect example

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
-import { Component } from 'react/cjs/react.production.min';
+import { useState } from 'react';
 import './App.css';
-import LifeCycleSample from './lifeCycleSample/LifeCycleSample';
-import ErrorBoundary from './lifeCycleSample/ErrorBoundary';
+import Info from './reactHook/info';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  state = {
-    color: '#000000'
-  }
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  }
-  render() {
+const App = () => {
+  const [visible, setVisible] = useState(false);
     return (
       <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
+        <button onClick={() => setVisible(!visible)}>{ visible? '숨기기':'보이기'}</button>
+        <hr/>
+        {visible && <Info />}
       </div>
     );
-  }
+  
 }
 
 export default App;
